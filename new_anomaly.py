7af69304dbb345ad729499c613309a3ee9d91080
LinirZamir <alzcorp@gmail.com>
2023-04-11 21:07:29 -0400

    init test

--- new_anomaly.py
+++ new_anomaly.py
@@ -4,30 +4,66 @@ import torch
 import numpy as np
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader, Dataset, random_split
 from torchvision import transforms
 import matplotlib.pyplot as plt
+import torch.nn.functional as F
+from torchvision.transforms import ToPILImage
+
+
+def compute_mean_std(folder):
+    num_pixels = 0
+    sum_pixel_values = 0
+    sum_squared_pixel_values = 0
+
+    image_files = os.listdir(folder)
+    for image_file in image_files:
+        img_path = os.path.join(folder, image_file)
+        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (224, 224))
+
+        num_pixels += img.size
+        sum_pixel_values += img.sum()
+        sum_squared_pixel_values += np.sum(np.square(img))
+
+    mean = sum_pixel_values / num_pixels
+    var = sum_squared_pixel_values / num_pixels - mean**2
+    var = np.clip(var, 0, None)  # Ensure the variance is non-negative
+    std = np.sqrt(var)
+
+    return mean, std
+
 
 # Custom dataset class
 class ImageDataset(Dataset):
-    def __init__(self, folder, transform=None):
+    # Add mean and std parameters for normalization
+    def __init__(self, folder, transform=None, mean=0.0, std=1.0):
         self.folder = folder
         self.transform = transform
+        self.mean = mean
+        self.std = std
         self.image_files = os.listdir(folder)
 
     def __len__(self):
         return len(self.image_files)
-
+    
     def __getitem__(self, index):
         img_path = os.path.join(self.folder, self.image_files[index])
+        
         img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
         img = cv2.resize(img, (224, 224))
 
-        if self.transform:
-            img = self.transform(img)
+        img = img / 255.0  # Normalize to the range of [0, 1]
+        img = img.astype(np.float32)
 
+        if self.transform:
+            img = self.transform(img)  # Apply the transform
+        else:
+            img = torch.tensor(img).unsqueeze(0)  # Convert the numpy array to a tensor
+            
         return img
-    
+
+
 # Variational Autoencoder model
 class VAE(nn.Module):
     def __init__(self):
@@ -65,6 +101,7 @@ class VAE(nn.Module):
         mu, logvar = self.fc_mu(h), self.fc_logvar(h)
         z = self.reparameterize(mu, logvar)
         x_recon = self.decoder_input(z).view(-1, 32, 56, 56)
+        x_recon = F.relu(x_recon)  # Add this line
         return self.decoder(x_recon), mu, logvar
 
 # VAE loss function
@@ -90,19 +127,61 @@ def train(model, dataloader, optimizer, device):
 
     return train_loss / len(dataloader)
 
+def validate(model, dataloader, device):
+    model.eval()
+    val_loss = 0.0
+
+    for batch in dataloader:
+        inputs = batch.to(device)
+        with torch.no_grad():
+            outputs, mu, logvar = model(inputs)
+            loss = vae_loss(outputs, inputs, mu, logvar)
+        val_loss += loss.item()
+
+    return val_loss / len(dataloader)
+
 # Main function
 def main():
     # Parameters
     epochs = 50
     batch_size = 16
     learning_rate = 1e-3
-    train_folder = 'hazelnut/train/good'
+    train_folder = 'transistor/train/good'
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        
+    patience = 10
+    best_val_loss = float('inf')
+    counter = 0
+
+    mean, std = compute_mean_std(train_folder)
 
     # Create dataset and dataloader
-    transform = transforms.Compose([transforms.ToTensor()])
-    dataset = ImageDataset(train_folder, transform=transform)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    transform = transforms.Compose([
+        transforms.ToPILImage(),  # Convert the ndarray to a PIL image
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(15),
+        transforms.RandomResizedCrop(224, scale=(0.9, 1.1), ratio=(0.9, 1.1)),
+        transforms.ToTensor(),
+    ])
+
+    dataset = ImageDataset(train_folder, transform=transform, mean=mean, std=std)
+
+    train_size = int(0.8 * len(dataset))
+    val_size = len(dataset) - train_size
+
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+
+    # Create dataloaders for training and validation
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    # Create dataset and dataloader for inference
+    inference_transform = transforms.Compose([
+        transforms.ToPILImage(),  # Convert the ndarray to a PIL image
+        transforms.ToTensor(),
+    ])
+
+
+    inference_dataset = ImageDataset(train_folder, transform=inference_transform, mean=mean, std=std)
 
     # Initialize model, loss, and optimizer
     model = VAE().to(device)
@@ -110,37 +189,78 @@ def main():
 
     # Train the model
     for epoch in range(epochs):
-        train_loss = train(model, dataloader, optimizer, device)
-        print(f'Epoch {epoch + 1}/{epochs}, Loss: {train_loss:.6f}')
+        train_loss = train(model, train_dataloader, optimizer, device)
+        val_loss = validate(model, val_dataloader, device)
+        print(f'Epoch {epoch + 1}/{epochs}, Train Loss: {train_loss:.6f}, Val Loss: {val_loss:.6f}')
+        # Early stopping
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            counter = 0
+            torch.save(model.state_dict(), 'vae.pth')
+        else:
+            counter += 1
+            print(f'Early stopping counter: {counter} out of {patience}')        
+            if counter >= patience:
+                print('Early stopping')
+                break
 
-    # Save the trained model
-    torch.save(model.state_dict(), 'vae.pth')
+    # Load the trained model
+    model.load_state_dict(torch.load('vae.pth'))
 
     # Inference on a test image
-    test_image_path = 'hazelnut/test/hole/005.png'
+    test_image_path = 'transistor/test/damaged_case/009.png'
     test_image = cv2.imread(test_image_path, cv2.IMREAD_GRAYSCALE)
     test_image = cv2.resize(test_image, (224, 224))
-    test_tensor = torch.tensor(test_image, dtype=torch.float32).unsqueeze(0).unsqueeze(0).to(device) / 255.0
 
-    # Get the reconstructed image
-    model.eval()
-    with torch.no_grad():
-        reconstructed_image, _, _ = model(test_tensor)
-        reconstructed_image = reconstructed_image.squeeze().cpu().numpy()
+    test_image = (test_image - mean) / (std + 1e-7)
+    test_image = test_image.astype(np.float32)
+
+    # Create a new transform for the test image
+    test_transform = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.ToTensor(),
+        transforms.Normalize((mean,), (std + 1e-7,)),  # Add normalization to the test_transform
+    ])
+
+
+    test_image_tensor = test_transform(test_image).unsqueeze(0).to(device)
+
+    diffs = []
+    for img in inference_dataset:
+        img_np = np.array(img)
+        img_tensor = torch.from_numpy(img_np).unsqueeze(0).to(device)
+
+        with torch.no_grad():
+            reconstructed_img, _, _ = model(img_tensor)
+            reconstructed_img_np = reconstructed_img.squeeze().cpu().numpy()  # Fix the variable name
+            diff = np.abs(img.cpu().numpy() - reconstructed_img_np)
+            diffs.append(diff)
+
+    diffs = np.array(diffs)
+    mean_diff = np.mean(diffs)
+    std_diff = np.std(diffs)
+
+
 
     # Calculate the difference between the original and reconstructed image
-    anomaly_map = np.abs(test_image / 255.0 - reconstructed_image)
 
     # Threshold the anomaly map
-    threshold = 0.1  # You can adjust this value based on your dataset
-    binary_map = np.uint8(anomaly_map > threshold)
+    threshold = mean_diff + 2 * std_diff
 
-    # Find contours in the binary map
-    contours, _ = cv2.findContours(binary_map, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Create anomaly map
+    with torch.no_grad():
+        reconstructed_test_image, _, _ = model(test_image_tensor)
+        reconstructed_test_image = reconstructed_test_image.squeeze().cpu().numpy()
+
+
+    test_image_normalized = test_image_tensor.squeeze().cpu().numpy()
+    anomaly_map = np.abs(test_image_normalized - reconstructed_test_image)
+    anomaly_map = (anomaly_map > threshold).astype(np.uint8)
 
     # Draw contours on the original image
     test_image_rgb = cv2.cvtColor(test_image, cv2.COLOR_GRAY2RGB)
-    cv2.drawContours(test_image_rgb, contours, -1, (255, 0, 0), 2)
+    contours, _ = cv2.findContours(anomaly_map, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(test_image_rgb, contours, -1, (0, 255, 0), 2)
 
     # Display the images
     plt.figure(figsize=(16, 4))
@@ -151,13 +271,14 @@ def main():
 
     plt.subplot(142)
     plt.title('Reconstructed Image')
-    plt.imshow(reconstructed_image, cmap='gray')
+    plt.imshow(reconstructed_test_image, cmap='gray')
 
     plt.subplot(143)
-    plt.title('Binary Map')
-    plt.imshow(binary_map, cmap='gray')
-
+    plt.title('Anomaly Map')
+    plt.imshow(anomaly_map, cmap='gray')
+    
     plt.show()
+
 
 if __name__ == '__main__':
     main()
